2c69f2da5b1748e72bf1050cf51d000930421700
“Simon <atlaspersson@outlook.com>
2025-02-21 12:52:19 +0100

    Minor tweaks

--- src/migrations.js
+++ src/migrations.js
@@ -1,2 +1,12 @@
 import mysql from 'mysql2/promise'
+import readFileSync from 'node:fs'
+import getProperties from 'properties-file'
 
+var propertiesFilename = '../db.properties'
+var properties = getProperties(readFileSync(propertiesFilename))
+const connection = await mysql.createConnection({
+    host: properties.DB_HOST,
+    user: properties.DB_USER,
+    password: properties.DB_PWD,
+    database: properties.DB_SCHEMA 
+})
